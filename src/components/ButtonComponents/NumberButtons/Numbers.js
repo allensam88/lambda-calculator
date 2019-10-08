3bff075fdd791779bb39eddc90d4740f64309807
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-10-08 15:19:23 -0700

    first commit, should have done this a long time ago

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
-// example of import from data.js. Note all the ../   This is how we move through folders. 
-/* 
-import { numbers } from '../../../data' 
-*/
+import NumberButton from './NumberButton'
 //Import your array data to from the provided data file
+// example of import from data.js. Note all the ../   This is how we move through folders. 
+import { numbers } from '../../../data'; 
 
-const Numbers = () => {
+const Numbers = (props) => {
   // STEP 2 - add the imported data to state
+  //const [numberState, setNumberState] = useState(numbers);
+  const [num, setNum] = useState(numbers);
+
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+      {/* STEP 3 - Use .map() to iterate over your array data and return a button component matching the name on the provided file. Pass it any props needed by the child component*/}
+       {num.map((item, index) => ( <NumberButton key={index} number={item} displayNums={props.displayNums} /> ))}
     </div>
   );
 };
+
+export default Numbers;
